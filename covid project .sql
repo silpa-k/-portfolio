1338e48ac5ca6f03a38341f81085ad91035260ac
silpa-k <162783975+silpa-k@users.noreply.github.com>
2024-03-09 12:51:05 +0530

    covid project .sql

--- covid project .sql
+++ covid project .sql
@@ -1,3 +1,9 @@
+/*
+Covid 19 Data Exploration 
+
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+
+*/
 Select *
 From pp..CovidDeaths
 
@@ -11,6 +17,8 @@ order by 3,4
 --From pp..CovidVaccinations
 --order by 3,4
 
+-- Select Data that we are going to be starting with
+
 Select Location, date, total_cases, new_cases, total_deaths, population
 From pp..CovidDeaths
 order by 1,2
@@ -20,18 +28,23 @@ From pp..CovidDeaths
 Where location like '%india%'
 and continent is not null 
 order by 1,2
-
+	
+-- Total Cases vs Population
  Select Location, date, Population, total_cases,  (total_cases/population)*100 as PercentPopulationInfected
 From pp..CovidDeaths
 Where location like '%states%'
 order by 1,2
 
+-- Countries with Highest Infection Rate compared to Population
+	
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From pp..CovidDeaths
 --Where location like '%states%'
 Group by Location, Population
 order by PercentPopulationInfected desc
-
+	
+-- Countries with Highest Death Count per Population
+	
 Select Location, MAX(cast(Total_deaths as int)) as TotalDeathCount
 From pp..CovidDeaths
 --Where location like '%states%'
@@ -39,15 +52,19 @@ Where continent is not null
 Group by Location
 order by TotalDeathCount desc
 
+-- BREAKING THINGS DOWN BY CONTINENT
 
-
+-- Showing contintents with the highest death count per population
+	
 Select continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
 From pp..CovidDeaths
 --Where location like '%states%'
 Where continent is not null 
 Group by continent
 order by TotalDeathCount desc
-
+	
+-- GLOBAL NUMBERS
+	
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
 From pp..CovidDeaths
 --Where location like '%states%'
@@ -55,7 +72,8 @@ where continent is not null
 --Group By date
 order by 1,2
 
-
+-- Total Population vs Vaccinations
+-- Shows Percentage of Population that has recieved at least one Covid Vaccine
 
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
@@ -67,7 +85,8 @@ Join pp..CovidVaccinations vac
 where dea.continent is not null 
 order by 2,3
 
-
+-- Using CTE to perform Calculation on Partition By in previous query
+	
 With PopvsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
 as
 (
@@ -84,7 +103,7 @@ where dea.continent is not null
 Select *, (RollingPeopleVaccinated/Population)*100 as Percentage 
 From PopvsVac
 
-
+-- for view
 Create View PPv as
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
